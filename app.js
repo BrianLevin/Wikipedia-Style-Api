fa4d08cbc7b90dc63223047498721eed98fb8808
BrianLevin <bml201095@gmail.com>
2021-06-15 21:52:11 -0400

    added a start script

--- app.js
+++ app.js
@@ -1,149 +1,145 @@
+
+
+
+// Creating app which utilizes the express package
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-// Creating app which utilizes the express package
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
-    title: String,
-    content: String
-  };
-
+  title: String,
+  content: String
+};
 
 const Article = mongoose.model("Article", articleSchema);
 
-
-///////////// Requests targeting all articles
+///////////////////////////////////Requests Targetting all Articles////////////////////////
 
 app.route("/articles")
 
-.get(function(req,res){
-    // query db and find documents inside articles collection
-        Article.find(function(err,foundArticles){
-    
-            if(!err){
-                // found articles
-                res.send(foundArticles)
-    
-            } else{
-                res.send(err)
-            }
-    
-        });
-    }).post(function (req,res){
-            // grab data sent through
-                console.log();
-                 console.log();
-            // document which will hold the collection values
-                 const newArticle = new Article ({
-                     title: req.body.title,
-                     content:req.body.content
-                 });
-                 // save post to db
-                 newArticle.save(function(err){
-                     if(!err){
-                     res.send("Sucessfully added a new article");
-                     } else{
-                         res.send(err);
-                     }
-                 });
-            
-            }).delete(function(req,res){
-                    Article.deleteMany(function(err){
-                        if (!err){
-                            res.send("succesfully deleted all articles.");
-                        } else {
-                            res.send(err)
-                        }
-                    });
-                });
+// query db and find documents inside articles collection
+.get(function(req, res){
+  Article.find(function(err, foundArticles){
+        // found articles
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
 
+.post(function(req, res){
+// grab data sent through
 
+   // document which will hold the collection values
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
 
+ // save post to db
+  newArticle.save(function(err){
+    if (!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
 
+.delete(function(req, res){
 
-///////////// Requests targeting all articles frequests targeting a specific article
-// app.route to get specofic articles
-app.route("/articles/:articleTitle")
-
-
-.get(function(req,res){
-
-   
-// look through collection of articles, find one document,  where title is = to request parameters
-Article.findOne({title:req.params.articleTitle }, function(err, foundArticle){
-if (foundArticle) {
-    res.send(foundArticle);
-} else{
-    res.send("no articles matching that title was found");
-}
-
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
-})
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
 
-.put(function(req,res){
-Article.update(
-    // look for article
-    {title:req.params.articleTitle },
+app.route("/articles/:articleTitle")
+// app.route to get specofic articles
+.get(function(req, res){
 
-    // update article
-    {title:req.body.title, content: req.body.content},
-// overwrite document what is epcified in the update
-    {overwrite: true},
-    function(err) {
-        if(!err) {
-            res.send("Sucessfully updated article")
-        }
+    // look through collection of articles, find one document,  where title is = to request parameters
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
     }
-
-)
-
+  });
 })
+
+.put(function(req, res){
+  // look for article
+  Article.update(
+    {title: req.params.articleTitle},
+
+// update article
+
+    {title: req.body.title, content: req.body.content},
+    // overwrite document what is epcified in the update
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
 // specific conditions to update
-.patch(function(req,res){
+.patch(function(req, res){
 
-    Article.update(
-        //condition
-        {title: req.params.articleTitle},
-
-        // specific conditions you want to update
-        {set: req.body},
-        function(err){
-            if(!err){
-                res.send("succcessfully updated articles")
-            } else{
-                res.send(err);
-            }
-        }
-    );
+  Article.update(
+       //condition
+    {title: req.params.articleTitle},
+     // specific conditions you want to update
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
 })
+
 // delete
-.delete(function(req,res){
-    Article.deleteOne(
-        {title:  req.params.articleTitle},
-        function(err){
-            if(!err){
-                res.send("successfully deleted articles")
+.delete(function(req, res){
 
-            } else{
-                res.send(err);
-            }
-        }
-    );
-})
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
 
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
-  
+  console.log("Server started on port 3000");
+});
